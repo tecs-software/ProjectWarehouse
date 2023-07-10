c0b351975c0a0ac7ea0900b7ad23ae04197b1fae
Maverick <Maverick@DESKTOP-6BP53A8>
2023-07-11 07:25:55 +0800

    live api

--- Controller/Create_api.cs
+++ Controller/Create_api.cs
@@ -32,12 +32,12 @@ namespace WarehouseManagement.Controller
         SuspiciousController suspiciouscontroller = new SuspiciousController();
         public async Task<bool> api_create(Receiver receiver, Booking_info booking_Info, bool suspicious)
         {
-            string url = "https://test-api.jtexpress.ph/jts-phl-order-api/api/order/create";
+            string url = "https://jtapi.jtexpress.ph/jts-phl-order-api/api/order/create";
             string key = Decrypt(GlobalModel.key);
             string logistics_interface = @"
             {
             ""actiontype"": ""add"",
-            ""environment"": ""staging:yes"",
+            ""environment"": ""production:yes"",
             ""eccompanyid"": ""THIRDYNAL"",
             ""customerid"": ""CS-V0234"",
             ""txlogisticid"": ""1547191-2707123"",
