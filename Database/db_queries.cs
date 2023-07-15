1ca2b5bec92d70207834833556baf467fa8caba5
Maverick <Maverick@DESKTOP-6BP53A8>
2023-07-15 22:34:00 +0800

    removed message boxes

--- Database/db_queries.cs
+++ Database/db_queries.cs
@@ -160,20 +160,15 @@ namespace WarehouseManagement.Database
             //        MessageBox.Show("dr" + dr[0].ToString());
             //    }
             //}
+
             sql.AddParam("@select_product", book_info.item_name);
             string product_id = sql.ReturnResult($"SELECT product_id FROM tbl_products WHERE item_name = @select_product ");
-            MessageBox.Show(product_id);
-            MessageBox.Show(book_info.item_name);
 
             string receiver_id = sql.ReturnResult($"SELECT receiver_id FROM tbl_receiver ORDER BY receiver_id DESC");
 
             string sender_id = sql.ReturnResult($"SELECT sender_id FROM tbl_products WHERE product_id = '{product_id}'");
 
             decimal total = decimal.Parse(book_info.quantity) * decimal.Parse(book_info.goods_value);
-
-           
-
-           
 
             sql.AddParam("@remarks", book_info.remarks);
 
