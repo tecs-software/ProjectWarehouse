69cf3d2cf1370424c7831bfd3340fcb59041bf51
Carl Argente <carlargente0156@gmail.com>
2023-07-15 22:07:13 +0800

    omg

--- Models/Booking_info.cs
+++ Models/Booking_info.cs
@@ -8,12 +8,12 @@ namespace WarehouseManagement.Models
 {
     public class Booking_info
     {
-        public string item_name { get; set; } = string.Empty;
+        public string item_name { get; set; }
         public string weight { get; set; } = string.Empty;  
         public string quantity { get; set; } = string.Empty;  
         public string goods_value { get; set; } = string.Empty;
         public string bag_specification { get; set; } = string.Empty;
-        public string remarks { get; set; } = string.Empty;
+        public string remarks { get; set; }
         public string courier { get; set; } = "J&T";
     }
 }
