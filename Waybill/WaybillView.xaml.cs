0a42949a21f1f01cdaf1df36717052cdffe1aadf
Maverick <Maverick@DESKTOP-6BP53A8>
2023-09-10 19:32:33 +0800

    waybill template latest layout

---
+++ Waybill/WaybillView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WarehouseManagement.Waybill
+{
+    public partial class WaybillView : Page
+    {
+        public WaybillView()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsFormsHost_Loaded(object sender, RoutedEventArgs e)
+        {
+            WaybillTemplates.LocalReport.ReportEmbeddedResource = "WarehouseManagement.Waybill.WaybillTemplate.rdlc";
+            WaybillTemplates.RefreshReport();
+        }
+    }
+}
